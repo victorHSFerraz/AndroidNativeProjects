df0e9ecb88b046200817c5288e5cf8c5b2e6d7c6
victorHSFerraz <victorf178@gmail.com>
2023-09-25 19:02:07 -0300

    navigation config

---
+++ DevMon/app/src/main/java/com/victor/devmon/CreaturesListFragment.kt
@@ -0,0 +1,17 @@
+package com.victor.devmon
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CreaturesListFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.creatures_list_fragment, container, false)
+    }
+}
--- DevMon/app/src/main/java/com/victor/devmon/NavigationActivity.kt
+++ DevMon/app/src/main/java/com/victor/devmon/NavigationActivity.kt
@@ -3,13 +3,27 @@ package com.victor.devmon
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NavigationActivity : AppCompatActivity() {
     private val navigationViewModel: NavigationViewModel by viewModels()
+
+    private val navController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigation_activity)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val graphInflater = navController.navInflater
+        val navGraph = graphInflater.inflate(R.navigation.nav_graph)
+        navController.graph = navGraph
     }
 }
